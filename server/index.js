2f5fb67614a537e068efb86cc4b5a9e266fa0021
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-03 09:11:21 -0500

    (init)

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+const port = 3000
+
+const statics = path.join(__dirname + '/../src/dist');
+
+app.use(express.static(statics));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`FEC Atelier App listening on port ${port}`)
+})
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      Hello World!
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
