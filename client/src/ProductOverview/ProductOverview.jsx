698c28479a366d9f8a910fe7107e124180716544
Anthony Bui <anthonyb50@gmail.com>
2023-03-06 19:26:59 -0500

    Added API key from github into env file. Imported through server

--- client/src/ProductOverview/ProductOverview.jsx
+++ client/src/ProductOverview/ProductOverview.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Axios from 'axios';
 import MainImageScreen from './components/MainImageScreen.jsx';
 import StylesSelection from './components/StylesSelection.jsx';
 import ProductInformation from './components/ProductInformation.jsx';
 
 const ProductOverview = (props) => {
+
+
   return (
     <div>
       <h1>Title</h1>
