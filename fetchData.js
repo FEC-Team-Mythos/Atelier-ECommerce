8ef5335a605f3a4b9659ea26bd9b3279ffeb8338
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-07 22:02:38 -0500

    finish API functionality

--- fetchData.js
+++ fetchData.js
@@ -1,10 +1,10 @@
 const config = require('./config.js')
 const axios = require('axios');
 
-const fetchData = (widget, method, params={}) => {
+const fetchData = (endpoint, params={}, method='get') => {
   axios({
-    method: "get",
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/${widget}/`,
+    method: method,
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${endpoint}/`,
     headers: {
       Authorization: `${config.TOKEN}`,
     },
@@ -12,8 +12,13 @@ const fetchData = (widget, method, params={}) => {
   })
   .then((response) => {
     console.log(response.data);
+    for (var i = 0; i < response.data.length; i++) {
+      console.log(response.data[i]);
+    }
   })
   .catch((err) => {
     console.log(err);
   })
 }
+
+module.exports = fetchData;
