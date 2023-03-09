f19982951bf7e393d39c728c720af0177cb00e0d
Anthony Bui <anthonyb50@gmail.com>
2023-03-08 19:46:16 -0500

    Fixed server to match main

--- server/index.js
+++ server/index.js
@@ -5,28 +5,6 @@ const fetch = require('../fetchData')
 const app = express()
 const port = 3000
 
-app.get('/specificProduct', (req, res) => {
-  fetch('/products/71701', {product_id:71701})
-  .then((data) => {
-    console.log('hey');
-  })
-})
-// axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/71701', {
-//   headers: {
-//     Authorization: process.env.API_KEY
-//   },
-//   params: {
-//     // count: 100
-//     product_id: 71701
-//   }
-// })
-// .then(data => {
-//   console.log(data.data);
-// })
-// .catch(err => {
-//   console.log(err, ' ERROR')
-// })
-
 const statics = path.join(__dirname + '/../client/dist');
 
 app.use(express.static(statics));
