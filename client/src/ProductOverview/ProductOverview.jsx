eaf68c6d133176ee1b01353a0bc76a84d51377ea
Anthony Bui <anthonyb50@gmail.com>
2023-03-09 16:26:15 -0500

    Functioning state for specific product data. Creating feature branches for remainder

--- client/src/ProductOverview/ProductOverview.jsx
+++ client/src/ProductOverview/ProductOverview.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import MainImageScreen from './components/MainImageScreen.jsx';
 import ProductInformation from './components/ProductInformation.jsx';
 import ProductDescription from './components/ProductDescription.jsx';
 
-const ProductOverview = (props) => {
+const ProductOverview = ({ request }) => {
 
+  const [product, setProduct] = useState({});
+  const [productStock, setProductStock] = useState({});
+
+  useEffect(()=> {
+    request('/products/71697', {product_id: 71697}, 'get')
+      .then(data => {
+        console.log(data.data);
+        setProduct(data.data);
+      })
+      .catch(err => {
+        console.log('Could not get: ', err);
+      })
+      .then(()=> (
+        request('/products/71697/styles', {product_id: 71697}, 'get')
+      ))
+      .then(data => {
+        console.log(data.data);
+        setProductStock(data.data);
+      })
+      .catch(err => {
+        console.log('Could not get: ', err);
+      })
+  }, [])
 
   return (
     <div>
