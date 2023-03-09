044bb6b1d979473822bc2bf6db8df5fd13bdc937
Anthony Bui <anthonyb50@gmail.com>
2023-03-08 20:36:32 -0500

    Completion of all route requests in server side

--- server/index.js
+++ server/index.js
@@ -10,17 +10,27 @@ const statics = path.join(__dirname + '/../client/dist');
 app.use(express.static(statics));
 app.use(express.json());
 
-/* EXAMPLE OF GET REQUEST USING FETCH HELPER FUNCTION
--------------------------------------------------------
-app.get('/products', (req, res) => {
-  fetch(req.url, {count: 100})
+app.get('*', (req, res) => {
+  fetch(req.url, req.body.params, req.method)
     .then(data => {
       res.send(data.data);
     })
     .catch(err => {
       console.log('Could not get data: ', err);
+      res.sendStatus(404);
     })
-}) */
+})
+
+app.post('*', (req, res) => {
+  fetch(req.url, req.body.params, req.method)
+    .then(data => {
+      res.sendStatus(201);
+    })
+    .catch(err => {
+      console.log('Could not post data: ', err);
+      res.sendStatus(501);
+    })
+})
 
 app.listen(port, () => {
   console.log(`FEC Atelier App listening on port ${port}`)
