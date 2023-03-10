ebf3b21f14d8ca9eb54719b049c869a56b1d2172
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-09 22:35:35 -0500

    Build initial chart structure for star ratings

--- client/src/Reviews/StarBox.jsx
+++ client/src/Reviews/StarBox.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const StarBox = (props) => {
+  const [chartRatings, setChartRatings] = useState({})
 
   const avgRating = (avg=0) => {
     props.allReviews.forEach((review) => {
@@ -16,11 +19,41 @@ const StarBox = (props) => {
     return avg / props.allReviews.length * 100 + '% of reviews recommend this product';
   }
 
+  const calcChartRatings = () => {
+    var newChart = {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0
+    }
+    props.allReviews.forEach((review) => {
+      console.log(review);
+      newChart[review.rating]++
+    })
+    setChartRatings(newChart);
+  }
+
+  const starGraph = () => {
+    return (
+      <BarChart
+        width={200}
+        height={200}
+        layout="vertical"
+        data={props.allReviews}>
+      <XAxis type="number"/>
+      <YAxis type="category" dataKey="rating" />
+      <Bar dataKey="rating" fill="#8884d8" />
+      </BarChart>
+    )
+  }
+
   return (
     <div>
       <div>{avgRating()} <span className="star">&#9733;</span>  </div>
       <div>
-        5/4/3/2/1 STAR CHART
+        <button onClick={()=>calcChartRatings()}>Hi</button>
+        {starGraph()}
       </div>
       <div>{avgRecommend()}</div>
       ----
