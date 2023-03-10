918756f278c5cad42610ca780ede4a663754fbf0
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-09 22:04:22 -0500

    Build basic Star functionality

--- client/src/Reviews/StarBox.jsx
+++ client/src/Reviews/StarBox.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const StarBox = () => {
+const StarBox = (props) => {
+
+  const avgRating = (avg=0) => {
+    props.allReviews.forEach((review) => {
+      avg += review.rating;
+    })
+    return avg / props.allReviews.length;
+  }
+
+  const avgRecommend = (avg=0) => {
+    props.allReviews.forEach((review) => {
+      review.recommend ? avg++ : null
+    })
+    return avg / props.allReviews.length * 100 + '% of reviews recommend this product';
+  }
+
   return (
     <div>
-
+      <div>{avgRating()} <span className="star">&#9733;</span>  </div>
+      <div>
+        5/4/3/2/1 STAR CHART
+      </div>
+      <div>{avgRecommend()}</div>
       ----
     </div>
   )
