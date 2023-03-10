bbe9bb4216270feec08881bb20e691dc4c5c8e8a
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-10 09:19:56 -0500

    Break Individual Reviews into Readable Tiles

--- client/src/Reviews/ReviewList.jsx
+++ client/src/Reviews/ReviewList.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReviewTile from './ReviewTile.jsx';
+import Filter from './Filter.jsx'
 
 const ReviewList = (props) => {
   return (
     <div id='reviewList'>
+    <Filter
+    allReviews={props.allReviews}
+    sortReviews={props.sortReviews}
+    />
+    --
     {props.allReviews.map((review) => {
       return(
         <ReviewTile
