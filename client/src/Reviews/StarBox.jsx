df6160dd74d39863f67a79560b6dbb3ea60287f7
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-12 09:57:17 -0400

    Refactor and clean up component

--- client/src/Reviews/StarBox.jsx
+++ client/src/Reviews/StarBox.jsx
@@ -1,39 +1,38 @@
-import React from 'react';
 import { useState, useEffect, useCallback } from 'react';
 import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, LabelList } from 'recharts';
 
-const StarBox = (props) => {
+const StarBox = ({ allReviews, filterParams, setFilterParams }) => {
   const [chartRatings, setChartRatings] = useState({})
 
   useEffect(() => {
     const chartData = [
-      { rating: 1, count: 0, total: props.allReviews.length},
-      { rating: 2, count: 0, total: props.allReviews.length},
-      { rating: 3, count: 0, total: props.allReviews.length},
-      { rating: 4, count: 0, total: props.allReviews.length},
-      { rating: 5, count: 0, total: props.allReviews.length},
+      { rating: 1, count: 0, total: allReviews.length},
+      { rating: 2, count: 0, total: allReviews.length},
+      { rating: 3, count: 0, total: allReviews.length},
+      { rating: 4, count: 0, total: allReviews.length},
+      { rating: 5, count: 0, total: allReviews.length},
     ];
 
-    props.allReviews.forEach((review) => {
+    allReviews.forEach((review) => {
       const rating = review.rating;
       chartData[rating - 1].count += 1;
     });
 
     setChartRatings(chartData);
-  }, [props.allReviews]);
+  }, [allReviews]);
 
   const avgRating = (avg=0) => {
-    props.allReviews.forEach((review) => {
+    allReviews.forEach((review) => {
       avg += review.rating;
     })
-    return avg / props.allReviews.length;
+    return avg / allReviews.length;
   }
 
   const avgRecommend = (avg=0) => {
-    props.allReviews.forEach((review) => {
+    allReviews.forEach((review) => {
       review.recommend ? avg++ : null
     })
-    return avg / props.allReviews.length * 100 + '% of reviews recommend this product';
+    return avg / allReviews.length * 100 + '% of reviews recommend this product';
   }
 
   const CustomLabel = ({ x, y, width, height, value, onClick, grabRating }) => {
@@ -81,22 +80,19 @@ const StarBox = (props) => {
     )
   }
 
-  //toggle rating filter
   const filterClick = useCallback((clickedRating) => {
-    console.log(clickedRating);
-    if (props.filterParams.includes(clickedRating)) {
-      props.setFilterParams(props.filterParams.filter(rating => rating !== clickedRating))
+    if (filterParams.includes(clickedRating)) {
+      setFilterParams(filterParams.filter(rating => rating !== clickedRating))
     } else {
-      props.setFilterParams([...props.filterParams, clickedRating]);
+      setFilterParams([...filterParams, clickedRating]);
       }
     },
-    [props.filterParams, props.setFilterParams]
+    [filterParams, setFilterParams]
   )
 
   const displayFilters = () => {
-    var sortedFilterParams = props.filterParams.sort().join(', ');
-
-    if (props.filterParams.length > 0) {
+    var sortedFilterParams = filterParams.sort().join(', ');
+    if (filterParams.length > 0) {
       return <div>
         Reviews filtered by {sortedFilterParams} stars
       </div>
