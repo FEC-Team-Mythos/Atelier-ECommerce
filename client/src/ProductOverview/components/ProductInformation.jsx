180bd835a3a4745dcc6fcf55145d32eca26431d3
Anthony Bui <anthonyb50@gmail.com>
2023-03-12 21:10:37 -0400

    Added rendering of price depending whether item is on sale or not

--- client/src/ProductOverview/components/ProductInformation.jsx
+++ client/src/ProductOverview/components/ProductInformation.jsx
@@ -8,7 +8,15 @@ const ProductInformation = ({productStock, product, setProductStock, productStyl
       <div>Reviews</div>
       <h3>{product.category}</h3>
       <h1>{product.name}</h1>
-      <h2>${productStock.original_price || product.default_price}</h2>
+      <h2>{productStock.name}</h2>
+      {productStock.sale_price ?
+        <div>
+          <h3>${productStock.original_price || product.default_price}</h3>
+          <h2>${productStock.sale_price}</h2>
+        </div>
+        :
+        <h3>${productStock.original_price || product.default_price}</h3>
+      }
       <StylesSelection productStock={productStock} setProductStock={setProductStock} productStyles={productStyles} setMainImage={setMainImage}/>
       <button>Add to Bag</button>
       <button>Favorite</button>
