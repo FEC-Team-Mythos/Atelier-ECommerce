52e63574be09be8f1165ec30488b5d7d79dbb407
Anthony Bui <anthonyb50@gmail.com>
2023-03-14 11:43:52 -0400

    Installed necessary testing packages

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
+import {render, screen} from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+
 describe('Random test examples', () => {
 
   test('adds 1 + 2 to equal 3', () => {
