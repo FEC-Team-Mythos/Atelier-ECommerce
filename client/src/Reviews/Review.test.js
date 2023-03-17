f42d33afeb0dcb8d88595a4ce19aaaffb795aed2
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-16 20:13:19 -0400

    Add basic tests, still async issues

--- client/src/Reviews/Review.test.js
+++ client/src/Reviews/Review.test.js
@@ -2,19 +2,35 @@ import React from 'react'
 import axios from 'axios'
 import '@testing-library/jest-dom'
 import {render, fireEvent, waitFor, screen} from '@testing-library/react'
+
 import Reviews from './Reviews.jsx';
 
-const request = (endpoint, params={}, method='get') => {
+const request = async (endpoint, params={}, method='get') => {
   return axios({
     method: method,
     url: endpoint,
     params: params
-  })
+  }).then((response) => response.data)
 }
 
 describe('Reviews', () => {
-  test('Review List should render to page', () => {
-    const { getByText } = render(<Reviews request={request}/>)
-    expect(getByText('Review Widget')).toBeInTheDocument();
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  test('Reviews should render to page', async () => {
+      const widget = render(<Reviews request={request}/>)
+      const reviewDiv = widget.container.querySelector('#reviews');
+      await waitFor(() => {
+        expect(reviewDiv.id).toEqual('reviews');
+      })
+  });
+
+  test('Reviews should render to 2 child components', async () => {
+      const widget = render(<Reviews request={request}/>)
+      const reviewDiv = widget.container.querySelector('#reviews');
+      await waitFor(() => {
+        expect(reviewDiv.children.length).toBe(2);
+      })
   });
 })
