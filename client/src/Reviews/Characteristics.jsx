d2b5c00ce7ec1af409e87594978f63d053a0425d
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-17 10:26:08 -0400

    Add basic labels to Sliders

--- client/src/Reviews/Characteristics.jsx
+++ client/src/Reviews/Characteristics.jsx
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 function Characteristics({ characteristics }) {
   const [characteristicRatings, setCharacteristicsRatings] = useState({});
+  const factorLabels = {
+    Size: ['Too Small', 'Too Big'],
+    Width: ['Too Narrow', 'Too Wide'],
+    Comfort: ['Uncomfortable', 'Perfect'],
+    Quality: ['Poor', 'Perfect'],
+    Length: ['Runs Short', 'Runs Long'],
+    Fit: ['Runs Tight', 'Runs Long']
+  }
 
   const marginCalc = (rating) =>
     // hardcoded value
@@ -18,11 +26,18 @@ function Characteristics({ characteristics }) {
             marginLeft: marginCalc(specificCharacteristic.value),
           };
 
+          console.log(factorLabels[specificCharacteristic.name][0])
+
           return (
-            <div id="total-characteristics-bar">
-              {/* Left */}
-              <span id={`${specificCharacteristic.name}-bar`} style={dotStyle} />
-              {/* Right */}
+            <div>
+              <span className='singular-character-title'>{specificCharacteristic.name}</span>
+            <div className='singular-character-slider'>
+              <span className={`${specificCharacteristic.name}-leftLabel`}>{factorLabels[specificCharacteristic.name][0]}</span>
+              <div id="total-characteristics-bar">
+                <span id={`${specificCharacteristic.name}-bar`} style={dotStyle} />
+              </div>
+              <span className={`${specificCharacteristic.name}-rightLabel`}>{factorLabels[specificCharacteristic.name][1]}</span>
+            </div>
             </div>
           );
         })
@@ -43,7 +58,7 @@ function Characteristics({ characteristics }) {
   }, [characteristics]);
 
   return (
-    <div>{characteristicsSlider()}</div>
+    <div id="characterSliders">{characteristicsSlider()}</div>
   );
 }
 
