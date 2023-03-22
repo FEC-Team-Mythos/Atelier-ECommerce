40026fe64bf3e2adc93a41284fe2af197b33e075
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-22 12:45:09 -0400

    Build functionality for custom hook to handle click events in document

---
+++ changeRequest.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+export function changeRequestHook(widget) {
+  const [clickedElement, setClickedElement] = useState(null);
+  const [clickedTime, setClickedTime] = useState(null);
+
+  useEffect(() => {
+    function handleClick(event) {
+      //set clicked div and time of click
+      setClickedElement(event.target.getAttribute('id'));
+      setClickedTime(new Date());
+    }
+
+    //listen for a click in the document
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      //remove event listener after click occurs
+      document.removeEventListener('click', handleClick);
+    };
+  }, []);
+
+  useEffect(() => {
+    //if state updates and click sets element and time, show values and send to API
+    if (clickedElement && clickedTime) {
+      console.log(`Clicked ${clickedElement.tagName} in ${widget} at ${clickedTime}`);
+    }
+  }, [clickedElement, clickedTime]);
+
+  return [clickedElement, clickedTime];
+}
