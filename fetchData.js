f258d4150e65ffb37d40fe1457d88d17191ceb2c
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-22 13:35:23 -0400

    Refactor FetchData to change Params to Data if request is POST

--- fetchData.js
+++ fetchData.js
@@ -1,15 +1,22 @@
-const config = require('./config.js')
 const axios = require('axios');
+const config = require('./config.js');
 
-const fetchData = (endpoint, params={}, method='get') => {
-  return axios({
-    method: method,
+const fetchData = (endpoint, params = {}, method = 'get') => {
+  const requestData = {
+    method,
     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp${endpoint}`,
     headers: {
-      Authorization: `${config.TOKEN}`
+      Authorization: `${config.TOKEN}`,
     },
-    params: params
-  })
-}
+  };
+
+  if (method === 'get') {
+    requestData.params = params;
+  } else {
+    requestData.data = params;
+  }
+
+  return axios(requestData);
+};
 
 module.exports = fetchData;
