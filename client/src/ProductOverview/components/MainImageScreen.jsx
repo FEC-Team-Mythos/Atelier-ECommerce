e999ae42208db254847088f4683a726f4ad62503
Anthony Bui <anthonyb50@gmail.com>
2023-03-22 18:15:03 -0400

    Cleaned up main image scrren based on ESLint

--- client/src/ProductOverview/components/MainImageScreen.jsx
+++ client/src/ProductOverview/components/MainImageScreen.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
 function MainImageScreen({ productInformation, mainImage, setMainImage }) {
@@ -5,7 +8,7 @@ function MainImageScreen({ productInformation, mainImage, setMainImage }) {
     <div>
       <ul>
         {productInformation.photos.map((photo, index) => (
-          <img src={photo.thumbnail_url} key={photo.url} alt="Product View Options" data-testid={`image-${index}`} onClick={() => {setMainImage(photo.url) }} />
+          <img src={photo.thumbnail_url} key={photo.url} alt="Product View Options" data-testid={`image-${index}`} onClick={() => { setMainImage(photo.url); }} />
         ))}
       </ul>
       <img src={mainImage} alt="Main Product" />
