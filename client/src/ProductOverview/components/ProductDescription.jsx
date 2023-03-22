655d3c206a7662b49578a289f162c78284b136eb
Anthony Bui <anthonyb50@gmail.com>
2023-03-22 18:18:20 -0400

    Cleaned up product description based on ESLint

--- client/src/ProductOverview/components/ProductDescription.jsx
+++ client/src/ProductOverview/components/ProductDescription.jsx
@@ -1,15 +1,18 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const ProductDescription = ({ product }) => {
+function ProductDescription({ product }) {
   return (
     <div>
       <div>{product.slogan}</div>
       <div>{product.description}</div>
-      {product.features.map(feature => (
-        <div key={feature.feature}>{feature.feature} : {feature.value}</div>
+      {product.features.map((feature) => (
+        <div key={feature.feature}>
+          {`${feature.feature}: ${feature.value}`}
+        </div>
       ))}
     </div>
-  )
+  );
 }
 
 export default ProductDescription;
