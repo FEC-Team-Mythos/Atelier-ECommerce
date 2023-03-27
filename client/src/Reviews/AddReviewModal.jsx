8a42d90370c1089df3797f1c554116b60157da4b
kahlstorf1 <dkahlstorf1@gmail.com>
2023-03-27 19:46:53 -0400

    Final tweak through Monday

--- client/src/Reviews/AddReviewModal.jsx
+++ client/src/Reviews/AddReviewModal.jsx
@@ -10,6 +10,15 @@ function AddReviewModal({ addReviewState, toggleAddReviewState, characteristics 
   const [formPhotos, setFormPhotos] = useState([]);
   const [formCharacteristics, setFormCharacteristics] = useState({});
 
+  const charLabels = {
+    Size: ['A size too small', '1/2 a size too small', 'Perfect', '1/2 a size too big', 'Too Big'],
+    Width: ['Too Narrow', 'Slightly Narrow', 'Perfect', 'Slightly Wide', 'Too Wide'],
+    Comfort: ['Uncomfortable', 'Slightly Uncomfortable', 'Ok', 'Comfortable', 'Perfect'],
+    Quality: ['Poor', 'Below Average', 'What I expected', 'Pretty Great', 'Perfect'],
+    Length: ['Runs Short', 'Runs slightly short', 'Perfect', 'Runs slightly long', 'Runs Long'],
+    Fit: ['Runs Tight', 'Runs slightly tight', 'Perfect', 'Runs slightly long', 'Runs Long'],
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const data = {
@@ -36,12 +45,19 @@ function AddReviewModal({ addReviewState, toggleAddReviewState, characteristics 
     }
   };
 
-  const DisplayRadioButtons = (input) =>{
+  function DisplayRadioButtons(input) {
     const radioButtons = [];
+    const [checkedChar, setCheckedChar] = useState(1);
+
     for (let i = 1; i <= 5; i++) {
       const radioButton = (
         <label key={i}>
-          <input type="radio" name={input} value={i} />
+          <input
+            type="radio"
+            name={input}
+            value={i}
+
+          />
           {i}
         </label>
       );
@@ -57,12 +73,14 @@ function AddReviewModal({ addReviewState, toggleAddReviewState, characteristics 
   const displayCharacterRadio = () => {
     if (characteristics) {
       const charKeys = Object.keys(characteristics);
+
       return (
         <>
           {charKeys.map((key) => (
             <div key={key}>
               {key}
-              <DisplayRadioButtons input={key} />
+              <DisplayRadioButtons input={key}/>
+              {charLabels[key][0]}
             </div>
           ))}
         </>
@@ -116,6 +134,7 @@ function AddReviewModal({ addReviewState, toggleAddReviewState, characteristics 
                   maxLength={60}
                   value={formSummary}
                   onChange={(e) => setFormSummary(e.target.value)}
+                  placeholder='Best purchase ever!'
                 />
               </div>
             </label>
@@ -128,10 +147,9 @@ function AddReviewModal({ addReviewState, toggleAddReviewState, characteristics 
                   maxLength={1000}
                   value={formBody}
                   onChange={(e) => setFormBody(e.target.value)}
+                  placeholder='Why did you like the product or not?'
                 />
               </div>
-            </label>
-            <label>
               <div>
                 Photos:
               </div>
@@ -144,6 +162,7 @@ function AddReviewModal({ addReviewState, toggleAddReviewState, characteristics 
                   maxLength={60}
                   value={formName}
                   onChange={(e) => setFormName(e.target.value)}
+                  placeholder='jackson11'
                 />
               </div>
             </label>
@@ -155,6 +174,7 @@ function AddReviewModal({ addReviewState, toggleAddReviewState, characteristics 
                   maxLength={60}
                   value={formEmail}
                   onChange={(e) => setFormEmail(e.target.value)}
+                  placeholder='jackson11@gmail.com'
                 />
               </div>
             </label>
