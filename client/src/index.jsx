124e099c8786bf353e58ea35f89f5186181cef67
Anthony Bui <anthonyb50@gmail.com>
2023-04-03 18:14:54 -0400

    Progress on routing. Bug somewhere

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,24 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ProductOverview from './ProductOverview/ProductOverview.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 import RelatedProducts from './RelatedProducts/RelatedProducts.jsx';
 import Reviews from './Reviews/Reviews.jsx';
 import CheckOut from './CheckOut/CheckOut.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { changeRequestHook } from '../../changeRequestHook.js';
 
-const App = () => {
-
-  const request = (endpoint, params={}, method='get') => {
-    return axios({
-      method: method,
-      url: endpoint,
-      params: params
-    });
-  };
+function App() {
+  const request = (endpoint, params = {}, method = 'get') => axios({
+    method,
+    url: endpoint,
+    params,
+  });
   /* EXAMPLE REQUEST WITHIN WIDGET. MAKE SURE TO EITHER CALL props.request OR { request }
   ___________________________________________________________________________________________
 
@@ -31,29 +28,31 @@ const App = () => {
     })
     */
 
-    const [outfits, setOutfits] = useState([]);
-    const [avgRating, setAvgRating] = useState(0);
-    const [totalReviewsPerProduct, setTotalReviewsPerProduct] = useState(0);
-    const [starArr, setStars] = useState('');
-    const [cartItems, setCartItems] = useState([]);
+  const [outfits, setOutfits] = useState([]);
+  const [avgRating, setAvgRating] = useState(0);
+  const [totalReviewsPerProduct, setTotalReviewsPerProduct] = useState(0);
+  const [starArr, setStars] = useState('');
+  const [cartItems, setCartItems] = useState([]);
 
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={
-          <div className="content">
-            <CheckOut cartItems={cartItems} />
-            <ProductOverview request={request} outfits={outfits} setOutfits={setOutfits} changeRequestHook={changeRequestHook} starArr={starArr} totalReviewsPerProduct={totalReviewsPerProduct} cartItems={cartItems} setCartItems={setCartItems}/>
-            <RelatedProducts  request={request}/>
-            <QuestionsAndAnswers  request={request}/>
-            <Reviews  request={request} changeRequestHook={changeRequestHook} starArr={starArr} setStars ={setStars} avgRating={avgRating} setAvgRating={setAvgRating} setTotalReviewsPerProduct={setTotalReviewsPerProduct}/>
-          </div>} />
-          <Route path='/checkout' element={<CheckOut cartItems={cartItems} />} />
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    <HashRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <div className="content">
+              <CheckOut cartItems={cartItems} />
+              <ProductOverview request={request} outfits={outfits} setOutfits={setOutfits} changeRequestHook={changeRequestHook} starArr={starArr} totalReviewsPerProduct={totalReviewsPerProduct} cartItems={cartItems} setCartItems={setCartItems} />
+              <RelatedProducts request={request} />
+              <QuestionsAndAnswers request={request} />
+              <Reviews request={request} changeRequestHook={changeRequestHook} starArr={starArr} setStars={setStars} avgRating={avgRating} setAvgRating={setAvgRating} setTotalReviewsPerProduct={setTotalReviewsPerProduct} />
+            </div>
+        )}
+        />
+        <Route path="/checkout" element={(<CheckOut cartItems={cartItems} />)} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 const container = document.getElementById('app');
